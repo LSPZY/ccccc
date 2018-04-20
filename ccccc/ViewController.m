7d3b2c874447596153df5dfd020c84f8576878bd
LSPZY <15203203178@163.com>
2018-04-20 14:00:23 +0800

    加了个peoplestr

--- ccccc/ViewController.m
+++ ccccc/ViewController.m
@@ -16,9 +16,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+-(void)setPeople:(NSString *)people
+{
+    people=@"hahahah";
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
