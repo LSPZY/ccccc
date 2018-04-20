101ca5d61f897f070aab75f14ee2297fad9bf989
LSPZY <15203203178@163.com>
2018-04-20 13:57:23 +0800

    Initial Commit

---
+++ ccccc/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ccccc
+//
+//  Created by ovov on 2018/4/20.
+//  Copyright © 2018年 bjovov. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "SVProgressHUD.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ccccc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ccccc
+//
+//  Created by ovov on 2018/4/20.
+//  Copyright © 2018年 bjovov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
